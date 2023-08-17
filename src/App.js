0176ed48ac416e6a4daad38424f4bcbda8a7f44f
Your Name <you@example.com>
2023-08-17 09:33:35 +0700

    fix image

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ function App() {
   if (bmi < 1) {
     imgSrc = null
   } else {
-    if(bmi < 25) {
+    if(bmi < 18.5) {
       imgSrc = require('../src/assets/underweight.png')
-    } else if (bmi >= 25 && bmi < 30) {
+    } else if (bmi >= 18.5 && bmi < 24.9) {
       imgSrc = require('../src/assets/healthy.png')
     } else {
       imgSrc = require('../src/assets/overweight.png')
